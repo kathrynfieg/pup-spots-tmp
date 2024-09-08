29ad4a4100916972f591d94d581926897d0081f8
Kathryn Fieg <kathrynfieg@gmail.com>
2024-09-09 08:15:01 +0930

    felx style

--- src/App.vue
+++ src/App.vue
@@ -116,6 +116,7 @@ async function signOutOfFirebase() {
 
 <template>
   <div style="width: 100%">
+    <!-- LOGOUT -->
     <div
       v-if="user?.email"
       style="display: flex; align-items: center; justify-content: space-between"
@@ -128,7 +129,7 @@ async function signOutOfFirebase() {
       >
     </div>
     <br />
-    <!-- <h1>Pup Spots Adelaide</h1> -->
+    <!-- TITLE -->
     <v-card
       class="mx-auto"
       color="surface-variant"
@@ -140,104 +141,124 @@ async function signOutOfFirebase() {
         <h1>Pup Spots Adelaide</h1>
       </template>
     </v-card>
-    <br />
-    <div v-if="!user?.email">
-      <h3>Sign In</h3>
-      <br />
-      <v-form>
-        <v-text-field
-          v-model="newUser.email"
-          hint="test@test.com"
-          type="email"
-          label="email"
-        ></v-text-field>
-        <v-text-field
-          v-model="newUser.password"
-          hint="password123"
-          type="password"
-          label="password"
-        ></v-text-field>
-        <div style="display: flex; gap: 1rem">
-          <v-btn color="green" @click.prevent="signInUser">Log in</v-btn>
-          <v-btn @click.prevent="createUser">Create User</v-btn>
-        </div>
-      </v-form>
-    </div>
-    <br />
-    <div v-if="user?.email">
-      <h3>Add pup spot</h3>
-      <br />
-      <v-form>
-        <v-text-field v-model="newSpot.name" label="Name"></v-text-field>
-        <v-text-field v-model="newSpot.type" label="Type"></v-text-field>
-        <v-text-field v-model="newSpot.address" label="Address"></v-text-field>
-        <v-text-field
-          v-model="newSpot.rating"
-          type="number"
-          label="Rating"
-        ></v-text-field>
-        <v-checkbox v-model="newSpot.favourite" label="Favourite"></v-checkbox>
-        <v-btn @click.prevent="addSpot">Add spot</v-btn>
-      </v-form>
-    </div>
-    <br />
-    <hr />
-    <br />
-    <div style="display: flex; flex-direction: column; gap: 1rem">
-      <v-card
-        v-for="spot in spotCollection"
-        :key="spot.name"
-        theme="dark"
-        border="1"
-        elevation="4"
-      >
-        <!-- <v-img
-          height="100"
-          cover
-          src="https://images.unsplash.com/photo-1622050956578-94fd044a0ada?q=80&w=1752&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        /> -->
-        <template #title>
-          <div
-            style="
-              display: flex;
-              align-items: center;
-              justify-content: space-between;
-            "
-          >
-            <div>{{ spot.name }}</div>
-            <v-icon
-              v-if="spot.favourite"
-              color="yellow-darken-2"
-              icon="mdi-star"
-            ></v-icon>
-          </div>
-        </template>
-        <template #subtitle> {{ spot.type }}</template>
 
-        <template #text>
-          Address: {{ spot.address }}
+    <!-- main content -->
+    <div style="display: flex; gap: 4rem; width: 100%">
+      <!-- Forms -->
+      <div style="min-width: 400px; margin-top: 2rem">
+        <div v-if="!user?.email">
+          <h3>Sign In</h3>
           <br />
-          Rating:
-          <div style="display: flex; gap: 1rem; align-items: center">
-            <v-rating
-              hover
-              readonly
-              :length="5"
-              :size="16"
-              :model-value="spot.rating"
-              active-color="primary"
-            />
-            <div style="margin-top: 0.5rem">{{ spot.rating }} / 5</div>
-          </div>
-        </template>
-
-        <v-card-actions v-if="user?.email">
-          <v-btn disabled>Edit</v-btn>
-          <v-btn variant="outlined" @click.prevent="() => deleteSpot(spot.id)"
-            >Delete</v-btn
+          <v-form>
+            <v-text-field
+              v-model="newUser.email"
+              hint="test@test.com"
+              type="email"
+              label="email"
+            ></v-text-field>
+            <v-text-field
+              v-model="newUser.password"
+              hint="password123"
+              type="password"
+              label="password"
+            ></v-text-field>
+            <div style="display: flex; gap: 1rem">
+              <v-btn color="green" @click.prevent="signInUser">Log in</v-btn>
+              <v-btn @click.prevent="createUser">Create User</v-btn>
+            </div>
+          </v-form>
+        </div>
+        <div v-else>
+          <h3>Add pup spot</h3>
+          <br />
+          <v-form>
+            <v-text-field v-model="newSpot.name" label="Name"></v-text-field>
+            <v-text-field v-model="newSpot.type" label="Type"></v-text-field>
+            <v-text-field
+              v-model="newSpot.address"
+              label="Address"
+            ></v-text-field>
+            <v-text-field
+              v-model="newSpot.rating"
+              type="number"
+              label="Rating"
+            ></v-text-field>
+            <v-checkbox
+              v-model="newSpot.favourite"
+              label="Favourite"
+            ></v-checkbox>
+            <v-btn @click.prevent="addSpot">Add spot</v-btn>
+          </v-form>
+        </div>
+      </div>
+      <!-- Spot List -->
+      <div style="width: 100%">
+        <div
+          style="
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+            margin-top: 2rem;
+          "
+        >
+          <v-card
+            v-for="spot in spotCollection"
+            :key="spot.name"
+            theme="dark"
+            border="1"
+            elevation="4"
           >
-        </v-card-actions>
-      </v-card>
+            <!-- <v-img
+              height="100"
+              cover
+              src="https://images.unsplash.com/photo-1622050956578-94fd044a0ada?q=80&w=1752&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            /> -->
+            <template #title>
+              <div
+                style="
+                  display: flex;
+                  align-items: center;
+                  justify-content: space-between;
+                "
+              >
+                <div>{{ spot.name }}</div>
+                <v-icon
+                  v-if="spot.favourite"
+                  color="yellow-darken-2"
+                  icon="mdi-star"
+                ></v-icon>
+              </div>
+            </template>
+            <template #subtitle> {{ spot.type }}</template>
+
+            <template #text>
+              Address: {{ spot.address }}
+              <br />
+              Rating:
+              <div style="display: flex; gap: 1rem; align-items: center">
+                <v-rating
+                  hover
+                  readonly
+                  :length="5"
+                  :size="16"
+                  :model-value="spot.rating"
+                  active-color="primary"
+                />
+                <div style="margin-top: 0.5rem">{{ spot.rating }} / 5</div>
+              </div>
+            </template>
+
+            <v-card-actions v-if="user?.email">
+              <v-btn disabled>Edit</v-btn>
+              <v-btn
+                variant="outlined"
+                @click.prevent="() => deleteSpot(spot.id)"
+                >Delete</v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </div>
+      </div>
     </div>
   </div>
 </template>
